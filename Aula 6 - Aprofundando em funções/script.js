70b31ba4cdc04b3f14a633159a805a51c104ca37
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-05 00:58:14 -0300

    tipos de funções

---
+++ Aula 6 - Aprofundando em funções/script.js
@@ -0,0 +1,17 @@
+function funcao(){
+    console.log('função padrão')
+}
+
+ funcao()
+
+ var funcao2 =  function(){
+     console.log('também funciona')
+ }
+
+ funcao2()
+
+ var arrowFunction = () =>{
+     console.log('arrow function também funciona')
+ }
+
+ arrowFunction()
