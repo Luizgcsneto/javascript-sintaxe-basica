2ed6317db81be3482170d49ed9875d9664422677
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-05 00:40:55 -0300

    inicio em funções

---
+++ Aula 5 - Funções e suas particulariddes/script.js
@@ -0,0 +1,12 @@
+function mensagem(){
+    console.log('mensagem já mocada')
+}
+
+mensagem()
+
+
+function mensagemRecebida(texto){
+    console.log(texto)
+}
+
+mensagemRecebida('mensagem recebida')
