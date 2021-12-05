43ae31e9115f9c84886d5c47c8c409d5ab2dc16f
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-05 02:28:53 -0300

    adicionando potencia e nova opção

--- Aula 6 - Aprofundando em funções/calculadora.js
+++ Aula 6 - Aprofundando em funções/calculadora.js
@@ -1,5 +1,5 @@
 function calculadora(){
-    const operacao = Number(prompt('escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - divisão (/)'))
+    const operacao = Number(prompt('escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - divisão (/)\n 5 - Potencialização (**)'))
 
     let num1 = Number(prompt('escolha o primeiro numero'))
     let num2 = Number(prompt('escolha o segundo numero'))
@@ -14,13 +14,20 @@ function calculadora(){
         resultado = num1 - num2
         alert(`${num1} - ${num2} = ${resultado}`)
     }
+
     function multiplicacao(){
         resultado = num1 * num2
         alert(`${num1} X ${num2} = ${resultado}`)
     }
+
     function divisao(){
         resultado = num1 / num2
         alert(`${num1} / ${num2} = ${resultado}`)
+    }
+
+    function potenciacao(){
+        resultado = num1 ** num2
+        alert(`${num1} elevado a ${num2}º = ${resultado}`)
     }
 
     if(operacao === 1){
@@ -33,6 +40,19 @@ function calculadora(){
     }
     else if( operacao === 4){
         divisao()
+    }
+    else if( operacao === 5){
+        potenciacao()
+    }
+
+    let novaOperacao = Number(prompt('deseja fazer uma nova operacao ? \n 1 - Sim \n 2 - Não'))
+
+    if(novaOperacao === 1){
+        calculadora()
+    } else if(novaOperacao === 2){
+        alert('até mais')
+    } else {
+        alert('erro digite uma opção válida')
     }
 }
 
